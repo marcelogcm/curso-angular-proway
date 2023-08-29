a4f1dc3a7b7934dc14bcd11d1e8a8510ef209f35
marcelogcm <marcelogcm@gmail.com>
2023-08-29 09:41:08 -0300

    commit lazy-loading

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
    { path:"segunda-pagina", component: SegundaPaginaComponent},
    { path:"",redirectTo: "primeira-pagina", pathMatch: "full"},
    { path:"pagina-com-parametro/:id", component: PaginaComParametroComponent},
+   { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
    { path:"**", component: PaginaNaoEncontradaComponent}
 ]
 
